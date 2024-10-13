bc672ddc328a7dc8845716ccac16fb03ac052adc
Jean-Emmanuel Perramant <jperrama@gmail.com>
2024-10-13 16:56:34 +0200

    setting tailwindcss with svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Chatbot</title>
+	<meta name="description" content="Mistral Chatbot demo" />
+</svelte:head>
+
+<section>
+	<h1 class="text-black">
+		Hello world
+	</h1>
+</section>
